17b5c00e46db31ed74f66583fd7be6f40c67a3d4
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-25 14:01:16 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,66 @@
+from pathlib import Path
+
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+BASE_DIR = Path(__file__).parent.resolve()
+README_FILE = BASE_DIR / "README.md"
+
+
+long_description = ""
+
+if README_FILE.is_file():
+    long_description = README_FILE.read_text(encoding="utf-8")
+
 
 setuptools.setup(
     name="cryptofetch",
     version="1.2.2",
     description="CLI tool to fetch and view cryptocurrencies prices",
-    url="https://github.com/codeswhite/cryptofetch",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
-    install_requires=[
-        'interutils',
-        'prettytable',
-        'requests',
-    ],
-    entry_points={
-        'console_scripts': [
-            'cryptofetch = cryptofetch:main',
-        ],
-    },
-    author="Max G",
-    author_email="max3227@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages()
+    url="https://github.com/codeswhite/cryptofetch",
+    author="Max G",
+    author_email="max3227@gmail.com",
+
+    python_requires=">=3.8",
+
+    packages=setuptools.find_packages(),
+
+    include_package_data=True,
+
+    install_requires=[
+        "interutils",
+        "prettytable",
+        "requests",
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "cryptofetch = cryptofetch:main",
+        ],
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Utilities",
+    ],
+
+    keywords=[
+        "crypto",
+        "cryptocurrency",
+        "cli",
+        "bitcoin",
+        "ethereum",
+        "market",
+        "terminal",
+    ],
+
+    project_urls={
+        "Source": "https://github.com/codeswhite/cryptofetch",
+    },
 )
